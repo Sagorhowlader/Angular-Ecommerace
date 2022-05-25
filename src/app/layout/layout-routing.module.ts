5b27836f247f0faee1460220c3635e48b930ea79
sagor2 <sagor@workspaceit.com>
2022-05-25 21:39:09 +0600

    code clean and about page added

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import {BaseComponent} from "./base/base.component";
 import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 
 const layoutRoutes: Routes = [
-  {path: '',
-    children: [
-      {path: 'home', component: HomeComponent}
-    ]
-  , component: BaseComponent}
+  {path: '',  redirectTo : '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+
 ]
 
 
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
-import {BaseComponent} from './base/base.component';
 import {RouterModule} from "@angular/router";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
@@ -11,13 +10,14 @@ import { HomeComponent } from './home/home.component';
 import { MatCarouselModule } from 'ng-mat-carousel';
 import { LayoutRoutingModule } from './layout-routing.module';
 import {MatBadgeModule} from "@angular/material/badge";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    BaseComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   exports: [
     HeaderComponent,
